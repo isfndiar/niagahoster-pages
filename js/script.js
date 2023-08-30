de8af6f406d74fd8e0a2ac43d6a0c0ed37385ab6
isfandiar Adi <141855809+isfndiar@users.noreply.github.com>
2023-08-30 07:01:41 +0700

    feat: searchbutton and dropdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // ketika naviigation di klikk
+let boxContent = document.querySelector('.box-content');
+
+const toggleBoxContent = document.querySelectorAll('#toggle');
+
+
+toggleBoxContent.forEach((btn) => {
+    btn.onclick = (e) => {
+      boxContent.classList.toggle('active');
+      e.preventDefault();
+    };
+  });
+
+
