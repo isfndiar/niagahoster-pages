9f27aee5e1540b6f690ae65fbb711be820d16581
isfandiar Adi <141855809+isfndiar@users.noreply.github.com>
2023-09-03 22:01:08 +0700

    menyelesaikan projek ( di bagian main content, ada sebagian belum responsive)

--- js/script.js
+++ js/script.js
@@ -15,9 +15,29 @@ toggleBoxContent.forEach((btn) => {
 const navbarNav = document.querySelector('.navbar-nav');
 const hb = document.querySelector('#hamburger-menu')
 
+document.querySelector('.hamburger-menu').onclick = (e) => {
+  navbarNav.classList.toggle('active');
+  e.preventDefault();
+}
 
-// document.addEventListener('click', function (e) {
-//   if(hb.contains(e.target)) {
-//     navbarNav.classList.toggle('active')
-//   }
-// })
+
+document.addEventListener('click', function (e) {
+  if (!navbarNav.contains(e.target) && !hb.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if (!aside.contains(e.target) && !ub.contains(e.target)) {
+    aside.classList.remove('active');
+  }
+
+
+})
+
+
+const aside = document.querySelector('.aside')
+const ub =  document.querySelector('.user-btn')
+
+document.querySelector('.user-btn').onclick = (e) => {
+  aside.classList.toggle('active');
+  e.preventDefault();
+}
